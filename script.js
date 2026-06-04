2713e8a9ddfc54048bfa01464034473b4e464760
kalpana-codes <prajapatikalpana881@gmail.com>
2026-06-04 17:21:11 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+window.addEventListener("scroll", function() {
+    let navbar = document.getElementById("navbar");
+
+    if (window.scrollY > 50) {
+        navbar.style.background = "blue";
+    } else {
+        navbar.style.background = "black";
+    }
+});
